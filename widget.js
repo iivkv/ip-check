5b3893be68e4d0977119edf2d0461f50949de6c4
iivkv <133651889+iivkv@users.noreply.github.com>
2026-04-16 20:51:18 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,5 +1,13 @@
 (function () {  
   function sendResult(ip, err) {    
     try {      
-      if (window.__ipWidgetCallback) {window.__ipWidgetCallback(ip, err);}} catch (e) {}  }
-  fetch('https://api64.ipify.org?format=json')    .then(function (response) {      if (!response.ok) throw new Error('HTTP ' + response.status);      return response.json();    })    .then(function (data) {      sendResult(data.ip);    })    .catch(function (err) {      sendResult(null, err && err.message ? err.message : String(err));    });})();
+      if (window.__ipWidgetCallback) {window.__ipWidgetCallback(ip, err);}
+    } catch (e) {}  
+  }
+  fetch('https://api64.ipify.org?format=json').then(function (response) {
+    if (!response.ok) throw new Error('HTTP ' + response.status);      
+     return response.json();
+  }).then(function (data) {
+    sendResult(data.ip);    
+  }).catch(function (err) {
+    sendResult(null, err && err.message ? err.message : String(err));    });})();
