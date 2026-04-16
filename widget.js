9b1228df1d30da36ef210daae5a295ef50760f16
iivkv <133651889+iivkv@users.noreply.github.com>
2026-04-16 21:40:53 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,16 +1,21 @@
-(function () {  
-  function sendResult(ip, err) {    
-    try {      
-      if (window.__ipWidgetCallback) {window.__ipWidgetCallback(ip, err);}
-    } catch (e) {}  
+(function () {
+  function sendResult(ip, err) {
+    try { window.__ipWidgetCallback && window.__ipWidgetCallback(ip, err); } catch (e) {}
   }
-  fetch('https://icanhazip.com').then(function (response) {
-    if (!response.ok) throw new Error('HTTP ' + response.status);      
-     return {"ip":response};
-  }).then(function (data) {
-    sendResult(data.ip);    
-  }).catch(function (err) {
-    sendResult(null, err && err.message ? err.message : String(err));
-  console.log('widget running');
-  });
+
+  fetch('https://icanhazip.com/')
+    .then(function (r) {
+      if (!r.ok) throw new Error('HTTP ' + r.status);
+      return r.text();
+    })
+    .then(function (txt) {
+      var ip = (txt || '').trim();
+      // simple validation (IPv4 or IPv6)
+      var ok = /^(\d{1,3}\.){3}\d{1,3}$/.test(ip) || /^[0-9a-fA-F:]+$/.test(ip);
+      if (!ok) throw new Error('Invalid IP format: ' + ip);
+      sendResult(ip);
+    })
+    .catch(function (err) {
+      sendResult(null, err && err.message ? err.message : String(err));
+    });
 })();
