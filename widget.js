c712a8c7841d42bb7ab578ccba42205ad64014e2
iivkv <133651889+iivkv@users.noreply.github.com>
2026-04-16 21:17:05 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -4,7 +4,7 @@
       if (window.__ipWidgetCallback) {window.__ipWidgetCallback(ip, err);}
     } catch (e) {}  
   }
-  fetch('https://api.myip.com').then(function (response) {
+  fetch('https://ip.seeip.org/json').then(function (response) {
     if (!response.ok) throw new Error('HTTP ' + response.status);      
      return response.json().ip;
   }).then(function (data) {
